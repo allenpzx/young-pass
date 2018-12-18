e3b28dd2a4dca6163914a264a3dd936a43beb9ad
彭子修 <pengzixiu@pengzixiudeMacBook-Air.local>
2018-12-18 10:07:20 +0800

    save

--- server/index.js
+++ server/index.js
@@ -19,53 +19,52 @@ const app = express();
 const PORT = 9093;
 const buildPath = require(path.resolve(__dirname, '../build/asset-manifest.json'));
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
+app.use(express.static(path.join(__dirname, '../build')));
 
-    // if(req.url !== '/'){
-    //     return res.sendFile(path.resolve(__dirname, '../build/index.html'));
-    // }
-    // console.log(req.url)
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+// app.get('*', (req, res, next) => {
 
-    const store = createStore(reducers);
-    store.dispatch({type: 'ADD'});
-    store.dispatch({type: 'ADD'});
+//     const store = createStore(reducers);
+//     store.dispatch({type: 'ADD'});
+//     store.dispatch({type: 'ADD'});
 
-    const context = {};
-    const rootElement = renderToString(
-        <Provider store={store}>
-            <StaticRouter
-                location={req.url}
-                context={context}
-            >
-                <App/>
-            </StaticRouter>
-        </Provider>
-    )
+//     const context = {};
+//     const rootElement = renderToString(
+//         <Provider store={store}>
+//             <StaticRouter
+//                 location={req.url}
+//                 context={context}
+//             >
+//                 <App/>
+//             </StaticRouter>
+//         </Provider>
+//     )
 
-    if (context.url) {
-        res.writeHead(301, {
-          Location: context.url
-        });
-        res.end();
-    } else {
+//     if (context.url) {
+//         res.writeHead(301, {
+//           Location: context.url
+//         });
+//         res.end();
+//     } else {
 
-        const finalState = store.getState();
-        // html.pipe(res, { end: false })
-        // html.on('end', () => {
-        //     res.write(`
-        //             </div>
-        //             <script src="${buildPath['main.js']}"></script>
-        //             <script>window.__PRELOADED_STATE__ = ${JSON.stringify(finalState).replace(/</g,'\\u003c')}</script>
-        //         </body>
-        //         </html>
-        //     `)
-        //     res.end()
-        // });
+//         const finalState = store.getState();
+//         // html.pipe(res, { end: false })
+//         // html.on('end', () => {
+//         //     res.write(`
+//         //             </div>
+//         //             <script src="${buildPath['main.js']}"></script>
+//         //             <script>window.__PRELOADED_STATE__ = ${JSON.stringify(finalState).replace(/</g,'\\u003c')}</script>
+//         //         </body>
+//         //         </html>
+//         //     `)
+//         //     res.end()
+//         // });
 
-        res.send(htmlTemplate('YoungPass学生特权卡', rootElement, buildPath, finalState));
-    }
-})
+//         res.send(htmlTemplate('YoungPass学生特权卡', rootElement, buildPath, finalState));
+//     }
+// })
 
 app.listen(PORT, (error) => {
     if (error) {
